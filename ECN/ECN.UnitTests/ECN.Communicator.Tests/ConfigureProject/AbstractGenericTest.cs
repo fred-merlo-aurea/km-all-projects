b1f069b1d6e24e6b8729b473bd29f894279722d5
Fred Merlo <fredmerlo@gmail.com>
2018-05-12 18:15:28 -0500

    Fix webservice and communicator projects for Webservice mock tests

---
+++ ECN/ECN.UnitTests/ECN.Communicator.Tests/ConfigureProject/AbstractGenericTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+
+namespace ECN_Framework_EntitiesTests.ConfigureProject
+{
+    [ExcludeFromCodeCoverage]
+    public abstract class AbstractGenericTest
+    {
+        protected static IFixture Fixture;
+
+        [SetUp]
+        public void Init()
+        {
+            if (Fixture == null)
+            {
+                Fixture = new Fixture().Customize(new AutoMoqCustomization());
+                Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            }
+        }
+    }
+}
